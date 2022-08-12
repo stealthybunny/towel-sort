0e094e2e13de5b90a3fc0e216b16e4d7e495fb14
Peter Zaichyk <zaichikpetr@gmail.com>
2022-08-12 17:51:05 +0300

    feat: completed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  if (matrix === null) {
+  if (matrix == null) {
     return [];
   }
   let l = matrix.length;
